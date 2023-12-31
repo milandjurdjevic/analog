3eabcc26bad39e1febc7256f316164341ee9a5a9
Milan Djurdjevic <milan_djurdjevic@icloud.com>
2023-12-31 13:58:47 +0100

    Change default pattern

--- src/Analog/Scanner.fs
+++ src/Analog/Scanner.fs
@@ -17,7 +17,7 @@ module Scanner =
 
     let private pattern =
         Regex(
-            "^(?<Timestamp>[\d\-]{10} [\d\:\.\+\ ]{19})? \[(?<Severity>[A-Z]{3})\] (?<Message>[\s\S]*?\n*(?=^[\d\-]{10}.*?(?:[^ \n]+ )|\z))",
+            "^\[(?<Timestamp>[\d\-]{10} [\d\:\.\+\ ]{19})?\] \[(?<Severity>[A-Z]{3})?\] (?<Message>[\s\S]*?\n*(?=^\[[\d\-]{10}.*?(?:[^ \n]+ )|\z))",
             RegexOptions.Multiline ||| RegexOptions.Compiled ||| RegexOptions.IgnoreCase
         )
 
