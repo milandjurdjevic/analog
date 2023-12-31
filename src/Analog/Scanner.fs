75e45db25ad9a4794da0f33324e4b9d2f390b7a8
Milan Djurdjevic <milan_djurdjevic@icloud.com>
2023-12-31 13:29:40 +0100

    Nest more code

--- src/Analog/Scanner.fs
+++ src/Analog/Scanner.fs
@@ -15,45 +15,45 @@ module Scanner =
     let private exactlyOfType<'a> enumerable =
         enumerable |> Seq.filter (fun a -> a.GetType() = typeof<'a>)
 
-    let private toDictionary (patternMatch: Match) =
-        patternMatch.Groups
-        |> Seq.filter (fun group -> group.GetType() = typeof<Group>)
-        |> Seq.map (fun group -> group.Name, group.Value)
-        |> readOnlyDict
-
-    let private pattern =
-        Regex(
-            "^(?<Timestamp>[\d\-]{10} [\d\:\.\+\ ]{19})? \[(?<Severity>[A-Z]{3})\] (?<Message>[\s\S]*?\n*(?=^[\d\-]{10}.*?(?:[^ \n]+ )|\z))",
-            RegexOptions.Multiline ||| RegexOptions.Compiled ||| RegexOptions.IgnoreCase
-        )
-
     let Scan (stream: Stream) =
         async {
+
+            let pattern =
+                Regex(
+                    "^(?<Timestamp>[\d\-]{10} [\d\:\.\+\ ]{19})? \[(?<Severity>[A-Z]{3})\] (?<Message>[\s\S]*?\n*(?=^[\d\-]{10}.*?(?:[^ \n]+ )|\z))",
+                    RegexOptions.Multiline ||| RegexOptions.Compiled ||| RegexOptions.IgnoreCase
+                )
+
             let rec scan (reader: StreamReader) (leftover: char array) (data: IReadOnlyDictionary<string, string> seq) =
                 async {
                     let buffer = Array.zeroCreate 1000
                     let! count = reader.ReadBlockAsync(buffer, 0, buffer.Length) |> Async.AwaitTask
                     let input = buffer |> Array.takeWhile notDefault |> Array.append leftover |> String
 
+                    let group (capture: Match) =
+                        capture.Groups
+                        |> exactlyOfType<Group>
+                        |> Seq.map (fun group -> group.Name, group.Value)
+                        |> readOnlyDict
+
                     if count = 0 && leftover.Length = 0 then
                         // There is nothing more to be processed, so just return the accumulated result.
                         return data
                     elif count = 0 && leftover.Length > 0 then
                         // Nothing is read from the stream, but there is still some characters left to be processed.
-                        return pattern.Matches input |> Seq.map toDictionary |> Seq.append data
+                        return pattern.Matches input |> Seq.map group |> Seq.append data
                     else
                         // Analyze input and pass the combined result to another iteration.
                         let matches = pattern.Matches input |> Array.ofSeq
 
-                        let nextLeftover =
+                        let leftover =
                             match matches |> Array.tryLast with
                             | None -> Array.empty
                             | Some value -> input[value.Index ..].ToCharArray()
 
-                        let nextData =
-                            matches[.. matches.Length - 2] |> Seq.map toDictionary |> Seq.append data
+                        let data = matches[.. matches.Length - 2] |> Seq.map group |> Seq.append data
 
-                        return! scan reader nextLeftover nextData
+                        return! scan reader leftover data
                 }
 
             use reader = new StreamReader(stream, Encoding.UTF8)
