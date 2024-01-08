a30fe48c9e649199b284dbdc6c29dd8cf8150d6d
Milan Djurdjevic <milan_djurdjevic@icloud.com>
2024-01-08 01:32:09 +0100

    Add new Analog.Console app written in F#

---
+++ src/Analog.Console/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.IO
+open System.Text.Json
+open System.Threading
+open Analog.Core
+open FSharp.Control
+open Spectre.Console
+open Spectre.Console.Json
+open Spectre.Console.Rendering
+
+let scan token stream = Scanner.Scan stream token
+
+let writeLine (element: IRenderable) =
+    AnsiConsole.Write element
+    AnsiConsole.WriteLine()
+
+let path = Environment.GetCommandLineArgs() |> Array.tryItem 1
+
+match path with
+| None -> FileStream.Null
+| Some value -> File.OpenRead value
+|> scan CancellationToken.None
+|> TaskSeq.map JsonSerializer.Serialize
+|> TaskSeq.map JsonText
+|> TaskSeq.iter writeLine
+|> ignore
