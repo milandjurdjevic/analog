8a12c00761553af65f627cfabe93f645ea09f073
milan.djurdjevic <milan.djurdjevic@anywhere365.net>
2024-02-13 16:30:16 +0100

    Adjust regex

--- src/Analog.Cli/Template.fs
+++ src/Analog.Cli/Template.fs
@@ -31,7 +31,7 @@ type Template =
         { Name = nameof Template.Quasar
           Regex =
             Regex(
-                "^\[(?<Timestamp>[\d\-]{10}\s[\d\:\.\+\ ]{8})?\s\s(?<Scope>[A-z0-9]+) (?<RequestId>[A-Z0-9]{13}:[0-9]{8})? (?<Severity>[A-Z]{3})?\] (?<Message>[\s\S]*?\n*(?=^\[[\d\-]{10}.*?(?:[^ \n]+ )|\z))",
+                "^\[(?<Timestamp>[\d\-]{10} [\d\:\.\+\ ]{8})?  (?<Scope>[A-z0-9]+)? (?<RequestId>[A-Z0-9]{13}:[0-9]{8})? (?<Severity>[A-Z]{3})?\] (?<Message>[\s\S]*?\n*(?=^\[[\d\-]{10}.*?(?:[^ \n]+ )|\z))",
                 RegexOptions.Multiline ||| RegexOptions.Compiled ||| RegexOptions.IgnoreCase
             )
           Highlighting =
