1bd4157c221cc12d09d3e7c94de8e75c8595a361
Milan Djurdjevic <milan_djurdjevic@icloud.com>
2024-02-19 20:43:39 +0100

    Add project metadata

--- src/Analog.Cli/Command.fs
+++ src/Analog.Cli/Command.fs
@@ -1,11 +1,28 @@
 namespace Analog.Cli
 
+open System
 open System.IO
 open System.Threading
 open System.Linq
+open System.Linq.Dynamic.Core
 open Spectre.Console
 open Spectre.Console.Cli
 open FSharp.Control
+
+type Settings() =
+    inherit CommandSettings()
+
+    [<CommandArgument(0, "[FILES]")>]
+    member val Files: string array = Array.empty with get, set
+
+    [<CommandOption("-f|--filter <FILTER>")>]
+    member val Filter: string = String.Empty with get, set
+
+    [<CommandOption("-s|--sortby <SORT_BY>")>]
+    member val SortBy: string = String.Empty with get, set
+
+    [<CommandOption("-t|--template <TEMPLATE>")>]
+    member val Template: string = String.Empty with get, set
 
 
 type Command() =
@@ -21,7 +38,6 @@ type Command() =
         | files when not (allExist files) -> ValidationResult.Error "One or more log files are not found."
         | _ -> ValidationResult.Success()
 
-
     override this.ExecuteAsync(_, settings) =
         task {
             let templates = Template.Init()
@@ -30,6 +46,18 @@ type Command() =
                 templates
                 |> Seq.tryFind (fun option -> option.Name = settings.Template)
                 |> Option.defaultValue (Seq.head templates)
+
+            let filter (source: IQueryable<'a>) =
+                if String.IsNullOrWhiteSpace settings.Filter then
+                    source
+                else
+                    source.Where(settings.Filter)
+
+            let sort (source: IQueryable<'a>) =
+                if String.IsNullOrWhiteSpace settings.SortBy then
+                    source
+                else
+                    source.OrderBy(settings.SortBy)
 
             let! logs =
                 taskSeq {
@@ -40,8 +68,8 @@ type Command() =
                 |> TaskSeq.toListAsync
 
             logs.AsQueryable()
-            |> Query.filter settings.Filter
-            |> Query.sort settings.SortBy
+            |> filter
+            |> sort
             |> Seq.toList
             |> Table.ofLogs
             |> AnsiConsole.Write
--- src/Analog.Cli/Log.fs
+++ src/Analog.Cli/Log.fs
@@ -1,4 +1,4 @@
-﻿namespace Analog.Cli
+﻿module Analog.Cli.Log
 
 open System
 open System.IO
@@ -11,57 +11,55 @@ open Microsoft.FSharp.Core
 open FSharp.Control
 open System.Linq
 
-module Log =
+let private parseDimensionValue (map: Map<string, string>) (dimension: Dimension) =
+    map.TryFind dimension.Name
+    |> Option.map dimension.Parse
+    |> Option.flatten
+    |> Option.defaultValue String.Empty
 
-    let private parseDimensionValue (map: Map<string, string>) (dimension: Dimension) =
-        map.TryFind dimension.Name
-        |> Option.map dimension.Parse
-        |> Option.flatten
-        |> Option.defaultValue String.Empty
+let private mapDimensions (dimensions: Dimension list) (map: Map<string, string>) =
+    dimensions
+    |> List.map (fun dimension -> dimension.Name, dimension |> parseDimensionValue map)
+    |> readOnlyDict
 
-    let private mapDimensions (dimensions: Dimension list) (map: Map<string, string>) =
-        dimensions
-        |> List.map (fun dimension -> dimension.Name, dimension |> parseDimensionValue map)
-        |> readOnlyDict
+let private mapRegexMatch (regexMatch: Match) =
+    regexMatch.Groups
+    |> Seq.filter (fun object -> object.GetType() = typeof<Group>)
+    |> Seq.map (fun group -> group.Name, group.Value)
+    |> Map.ofSeq
 
-    let private mapRegexMatch (regexMatch: Match) =
-        regexMatch.Groups
-        |> Seq.filter (fun object -> object.GetType() = typeof<Group>)
-        |> Seq.map (fun group -> group.Name, group.Value)
-        |> Map.ofSeq
+let private charMemoryToString (memory: Memory<char>) =
+    memory.ToArray()
+    |> Array.filter (fun char -> char <> Unchecked.defaultof<char>)
+    |> String
 
-    let charMemoryToString (memory: Memory<char>) =
-        memory.ToArray()
-        |> Array.filter (fun char -> char <> Unchecked.defaultof<char>)
-        |> String
+let ofStream (template: Template) ([<EnumeratorCancellation>] token: CancellationToken) (stream: Stream) =
+    taskSeq {
+        use reader = new StreamReader(stream, Encoding.UTF8)
+        let batchSizeMax = 1000
+        let mutable batchSize = batchSizeMax
+        let mutable leftover = String.Empty
 
-    let ofStream (template: Template) ([<EnumeratorCancellation>] token: CancellationToken) (stream: Stream) =
-        taskSeq {
-            use reader = new StreamReader(stream, Encoding.UTF8)
-            let batchSizeMax = 1000
-            let mutable batchSize = batchSizeMax
-            let mutable leftover = String.Empty
+        let regex =
+            Regex(template.Regex, RegexOptions.Compiled ||| RegexOptions.Multiline ||| RegexOptions.IgnoreCase)
 
-            let regex =
-                Regex(template.Regex, RegexOptions.Compiled ||| RegexOptions.Multiline ||| RegexOptions.IgnoreCase)
+        while batchSize = batchSizeMax && not token.IsCancellationRequested do
+            let memory = Memory<char>(Array.zeroCreate batchSizeMax)
+            let! blockSize = reader.ReadBlockAsync(memory, token)
+            batchSize <- blockSize
 
-            while batchSize = batchSizeMax && not token.IsCancellationRequested do
-                let memory = Memory<char>(Array.zeroCreate batchSizeMax)
-                let! blockSize = reader.ReadBlockAsync(memory, token)
-                batchSize <- blockSize
+            let input = leftover + charMemoryToString memory
+            let matches = regex.Matches input |> Array.ofSeq
 
-                let input = leftover + charMemoryToString memory
-                let matches = regex.Matches input |> Array.ofSeq
+            for regexMatch in matches.SkipLast 1 do
+                yield regexMatch |> mapRegexMatch |> mapDimensions template.Dimensions
 
-                for regexMatch in matches.SkipLast 1 do
+            leftover <-
+                match matches |> Seq.tryLast with
+                | None -> String.Empty
+                | Some value -> input[value.Index ..]
+
+            if batchSize < batchSizeMax then
+                for regexMatch in regex.Matches leftover do
                     yield regexMatch |> mapRegexMatch |> mapDimensions template.Dimensions
-
-                leftover <-
-                    match matches |> Seq.tryLast with
-                    | None -> String.Empty
-                    | Some value -> input[value.Index ..]
-
-                if batchSize < batchSizeMax then
-                    for regexMatch in regex.Matches leftover do
-                        yield regexMatch |> mapRegexMatch |> mapDimensions template.Dimensions
-        }
+    }
