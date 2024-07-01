ad7f1e7907c67cb5d248f76f856394759e0fd490
milan.djurdjevic <milan_djurdjevic@icloud.com>
2024-07-01 03:23:32 +0200

    Add Unit Test for toIterable

--- src/Analog.Tests/TemplatesTest.fs
+++ src/Analog.Tests/TemplatesTest.fs
@@ -1,5 +1,7 @@
 ﻿module Analog.Tests.TemplatesTest
 
+open System.IO
+open System.Text.RegularExpressions
 open Xunit
 open FsUnit.Xunit
 
@@ -7,3 +9,9 @@ open FsUnit.Xunit
 let ``toMap returns a non-empty map`` () =
     let templates = Analog.Templates.toMap ()
     templates |> should not' <| be Empty
+    
+[<Fact>]
+let ``toIterable returns an iterable with a multiline regex`` () =
+    let template = Analog.Templates.Template()
+    let iterable = template |> Analog.Templates.toIterable Stream.Null 
+    iterable.Regex.Options |> should equal RegexOptions.Multiline
