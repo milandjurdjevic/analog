49b6a9146b9c2a23900e7c5c851ec2573fbeba6f
Milan Djurdjevic <milan_djurdjevic@icloud.com>
2024-08-19 00:34:57 +0200

    Remove False Test Case

--- src/Analog.Tests/FilterTest.fs
+++ src/Analog.Tests/FilterTest.fs
@@ -6,7 +6,6 @@ open Xunit
 [<Theory>]
 [<InlineData("name = 'value'")>]
 [<InlineData("has18 = true | age > 17")>]
-[<InlineData("has18 == true | age > 17")>]
 let ``Parse valid expression`` input =
     match Parser.parse input with
     | Ok value -> value.ToString() |> Snapshot.compare |> Snapshot.input [|input|] |> Snapshot.exec
