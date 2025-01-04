bab75469440b32011a7d3b85dedd605300fe11a8
Milan Djurdjevic <71662228+milandjurdjevic@users.noreply.github.com>
2025-01-04 23:01:23 +0100

    Grok Parser (#3)
    - Parse log entries using Grok parser.
    - Use only CLI arguments for configuring applications.

--- src/Analog/Program.fs
+++ src/Analog/Program.fs
@@ -1,52 +1,74 @@
 ﻿open System
 open System.IO
+open System.Text.Json
 open Analog
 open Argu
+open Spectre.Console
+open Spectre.Console.Json
 
 type Argument =
     | [<MainCommand; Mandatory; First>] File of string
-    | [<AltCommandLine("-t"); Unique>] Template of string
+    | [<AltCommandLine("-p"); Unique>] Pattern of string
     | [<AltCommandLine("-f")>] Filter of string
 
     interface IArgParserTemplate with
         member this.Usage =
             match this with
-            | File _ -> "log file path."
-            | Template _ -> "log file template."
-            | Filter _ -> "log file filter."
+            | File _ -> "Log file path."
+            | Pattern _ -> "GROK pattern."
+            | Filter _ -> "Filter expression."
 
-let eval exp (log: Map<string, string>) =
-    log |> Map.map (fun _ v -> v :> obj) |> Filter.Evaluator.eval exp
+let import files =
+    files |> List.map File.ReadAllText |> List.reduce (fun all next -> all + next)
 
-let printJson check log =
-    if check log then
-        Print.json log
+let parse pattern text =
+    pattern
+    |> Option.map EntryParser.create
+    |> Option.defaultValue (EntryParser.value |> Result.Ok)
+    |> Result.map (EntryParser.parse text)
 
-try
-    let arg =
+let filter filter entries =
+    filter
+    |> Option.map (ParserRunner.run FilterParser.expression)
+    |> Option.map (fun res ->
+        res
+        |> Result.bind (fun filter -> entries |> Result.map (fun entries -> filter, entries)))
+    |> Option.map (fun result ->
+        result
+        |> Result.map (fun (filter, entries) ->
+            entries |> List.filter (fun entry -> FilterEvaluator.evaluate entry filter)))
+    |> Option.defaultValue entries
+
+let handle (args: ParseResults<Argument>) =
+    import (args.GetResults Argument.File)
+    |> parse (args.TryGetResult Argument.Pattern)
+    |> filter (args.TryGetResult Argument.Filter)
+
+let normalize (entry: Entry) =
+    entry
+    |> Map.map (fun _ value ->
+        match value with
+        | String value -> box value
+        | Number value -> box value
+        | Boolean value -> box value
+        | Timestamp value -> box value)
+
+let print entries =
+    entries
+    |> List.map normalize
+    |> JsonSerializer.Serialize
+    |> JsonText
+    |> AnsiConsole.Write
+
+let args =
+    try
         ArgumentParser
             .Create<Argument>()
             .ParseCommandLine(Environment.GetCommandLineArgs() |> Array.skip 1)
+        |> Result.Ok
+    with err ->
+        Result.Error err.Message
 
-    let template =
-        arg.TryGetResult Template
-        |> Option.map (fun template -> Template.configuration |> Map.find template)
-        |> Option.defaultValue (Template.configuration |> Seq.head |> _.Value)
-
-    let filter = arg.TryGetResult Filter |> Option.map Filter.Parser.parse
-
-    let iter handler =
-        let next file =
-            use stream = File.OpenRead file
-            Extract.stream handler template.Pattern stream
-
-        arg.GetResults File |> Seq.iter next
-
-    match filter with
-    | None -> iter Print.json
-    | Some flt ->
-        match flt with
-        | Ok exp -> iter <| (printJson <| eval exp)
-        | Error err -> err |> Print.error
-with :? ArguParseException as exc ->
-    exc.Message |> eprintfn "%s"
+match args |> Result.bind handle with
+| Ok entries -> print entries
+| Error error -> error |> eprintf "%s"
