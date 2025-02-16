a2b2ede2d5607692707f531a2aee7963f859861a
Milan Djurdjevic <milan_djurdjevic@icloud.com>
2025-02-16 01:46:56 +0100

    Async Batch

--- core/src/Batch.fs
+++ core/src/Batch.fs
@@ -4,40 +4,58 @@ open System
 open System.IO
 open System.Text.RegularExpressions
 open System.Diagnostics.CodeAnalysis
+open FSharp.Control
 
-type Props = { Delimiter: Regex; Buffer: int }
+type Capture = string -> string seq * string
 
-let setup ([<StringSyntax("regex")>] start) buffer =
-    { Delimiter = Regex(start, RegexOptions.Multiline ||| RegexOptions.Compiled, TimeSpan.FromSeconds(int64 5))
-      Buffer = buffer }
+let capture ([<StringSyntax("regex")>] regex) : Capture =
 
-let load (stream: Stream) props : string seq =
+    let regex =
+        Regex(regex, RegexOptions.Multiline ||| RegexOptions.Compiled, TimeSpan.FromSeconds(int64 5))
+
+    fun input ->
+        let entries, index =
+            ((List.empty<string>, 0), regex.Matches input)
+            ||> Seq.fold (fun (list, index) item ->
+                let entry = input[index .. item.Index - 1].Trim()
+                (if entry.Length > 0 then list @ [ entry ] else list), item.Index)
+
+        match entries with
+        | [] -> [], input
+        | entries -> entries, input[index..]
+
+let sync (stream: Stream) (capture: Capture) : string seq =
     seq {
         use reader = new StreamReader(stream)
         let mutable leftover = String.Empty
 
         while not reader.EndOfStream do
-            // Read a chunk of data from the stream.
-            let buffer = Array.zeroCreate<char> props.Buffer
-            let block = reader.ReadBlock(buffer)
-            let content = leftover + (buffer |> Array.take block |> String.Concat)
-            let matches = props.Delimiter.Matches(content)
+            let buffer = Array.zeroCreate<char> 1024
+            let block = reader.ReadBlock buffer
+            let cap = leftover + (buffer |> Array.take block |> String.Concat) |> capture
+            leftover <- snd cap
+            yield! fst cap
 
-            // Collect the matches found in the chunk and the last match index.
-            let entries, index =
-                ((List.empty<string>, 0), matches)
-                ||> Seq.fold (fun (list, index) item ->
-                    let entry = content[index .. item.Index - 1].Trim()
-                    (if entry.Length > 0 then list @ [ entry ] else list), item.Index)
+        yield!
+            match leftover.Trim() with
+            | "" -> Seq.empty
+            | leftover -> Seq.singleton leftover
+    }
 
-            // Store any leftover content for the next iteration.
-            leftover <- if entries.Length > 0 then content[index..] else content
+let async (stream: Stream) (capture: Capture) =
+    taskSeq {
+        use reader = new StreamReader(stream)
+        let mutable leftover = String.Empty
 
-            // Yield each entry found in the current chunk, separately.
-            yield! entries
+        while not reader.EndOfStream do
+            let buffer = Array.zeroCreate<char> 1024
+            let! block = reader.ReadBlockAsync(Memory(buffer))
+            let cap = leftover + (buffer |> Array.take block |> String.Concat) |> capture
+            leftover <- snd cap
+            yield! fst cap
 
-        let last = leftover.Trim()
-        // Ensure the last entry is not lost.
-        if last.Length > 0 then
-            yield last
+        yield!
+            match leftover.Trim() with
+            | "" -> Seq.empty
+            | leftover -> Seq.singleton leftover
     }
